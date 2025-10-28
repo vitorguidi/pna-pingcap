cfa2d439ac4d3f81d3d6b700738b4db85541c765
Vitor Guidi <vitorguidi@gmail.com>
2025-10-27 23:08:39 -0300

    Turn into lib with boilerplate tests

---
+++ src/kv.rs
@@ -0,0 +1,21 @@
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        todo!();
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        todo!();
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        todo!();
+    }
+
+    pub fn remove(&mut self, key: String) {
+        todo!();
+    }
+}
+
+fn main() {}
